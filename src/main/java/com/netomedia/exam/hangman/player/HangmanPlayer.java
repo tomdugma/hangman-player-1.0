a0ef550a19047b08ccf826be861cfdf9261f5dc4
tom dugma <dugma@post.bgu.ac.il>
2022-05-22 18:29:14 +0300

    docs

--- src/main/java/com/netomedia/exam/hangman/player/HangmanPlayer.java
+++ src/main/java/com/netomedia/exam/hangman/player/HangmanPlayer.java
@@ -20,47 +20,48 @@ public class HangmanPlayer {
      * To start a new game call server.startNewGame()
      */
     public static void main(String[] args) throws Exception {
-        Hashtable< Integer, String > map = new Hashtable< Integer, String >();
+
+        List<String> words = readDictionary("C:\\Users\\User\\Downloads\\hangman-player-1.0\\hangman-player-1.0\\src\\main\\resources\\dictionary.txt");
+
+        Map<Integer,List<String>> lengthPerWord = words.stream().collect(Collectors.groupingBy(word -> word.length()));
+
+        // init game
+        ServerResponse serverResponse1 = server.startNewGame();
+        token = serverResponse1.getToken();
+        int sizeWord = serverResponse1.getHangman().length();
+
+        // first filter, by word length
+        List<String> filterCurrWordsList = lengthPerWord.get(sizeWord);
+
+        // get updated map
+        Hashtable<Character, Integer> mostCommonChar = getUpdatedMap(filterCurrWordsList,null);
+        while(!serverResponse1.isGameEnded()){
+            Character chosenCharacterToGuess = mostCommonChar.entrySet().stream().max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get().getKey();
+            ServerResponse serverResponse = server.guess(token,chosenCharacterToGuess.toString());
+            token = serverResponse.getToken(); // update token
+
+            if (serverResponse.isCorrect() == null) // for some reason correct = null in some cases
+                continue;
+            if(!serverResponse.isCorrect())
+            {
+                mostCommonChar.remove(chosenCharacterToGuess);
+                filterCurrWordsList =  filterCurrWordsList.stream().filter(w -> !(w.contains(chosenCharacterToGuess.toString()))).collect(Collectors.toList());
+            }else{
+                filterCurrWordsList =  filterCurrWordsList.stream().filter(w -> w.contains(chosenCharacterToGuess.toString())).collect(Collectors.toList());
+                mostCommonChar = getUpdatedMap(filterCurrWordsList,chosenCharacterToGuess);
+            }
+        }
+        System.out.println("END");
+    }
+
+    public static List<String> readDictionary(String path) throws IOException {
         BufferedReader br = new BufferedReader( new FileReader ("C:\\Users\\User\\Downloads\\hangman-player-1.0\\hangman-player-1.0\\src\\main\\resources\\dictionary.txt"));
         String line = "";
         List<String> words = new ArrayList<>();
         while ((line = br.readLine()) != null) {
             words.add(line);
         }
-        Map<Integer,List<String>> lengthPerWord = words.stream().collect(Collectors.groupingBy(word -> word.length()));
-
-        ServerResponse serverResponse1 = server.startNewGame();
-        token = serverResponse1.getToken();
-        int sizeWord = serverResponse1.getHangman().length();
-
-        List<String> filterCurrWordsList = lengthPerWord.get(sizeWord);
-        Hashtable<Character, Integer> mostCommonChar = getUpdatedMap(filterCurrWordsList,null);
-
-        while(!serverResponse1.isGameEnded()){
-            Character chosenCharacterToGuess = mostCommonChar.entrySet().stream().max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get().getKey();
-            ServerResponse serverResponse = server.guess(token,chosenCharacterToGuess.toString());
-            if (serverResponse.isCorrect() == null)
-                continue;
-            if(!serverResponse.isCorrect())
-            {
-                mostCommonChar.remove(chosenCharacterToGuess);
-                token = serverResponse.getToken(); // update token
-            }else{
-                token = serverResponse.getToken();
-                filterCurrWordsList =  filterCurrWordsList.stream().filter(w -> w.contains(chosenCharacterToGuess.toString())).collect(Collectors.toList());
-                mostCommonChar = getUpdatedMap(filterCurrWordsList,chosenCharacterToGuess);
-            }
-        }
-        System.out.println("END");
-        // 1 .filter all the word according to size of the word
-        // 2. guess the most common char in the eng lang
-        // 3. say that we found one char  (h)
-        // 3.1 search the char that appear in most words with 'h'
-        //
-
-
-        // load the file as hash table
-        //
+        return words;
     }
 
     public static Hashtable<Character, Integer> getUpdatedMap(List<String> filterWordsByLength,Character excludeLetter)
@@ -82,19 +83,4 @@ public class HangmanPlayer {
         return mostCommonChar;
     }
 
-    public static Hashtable<Character, Integer> getUpdatedMap2(List<String> filterWordsByLength,String currFound)
-    {
-        Hashtable<Character, Integer> mostCommonChar = new Hashtable<>();
-        for(String word:filterWordsByLength){
-
-            char[] charArray = word.toCharArray();
-            Character[] charObjectArray = ArrayUtils.toObject(charArray);
-
-            Set<Character> charSet = new HashSet<Character>(Arrays.asList(charObjectArray));
-            for (Character c: charSet){
-                mostCommonChar.put(c, mostCommonChar.getOrDefault(c,0)+1);
-            }
-        }
-        return mostCommonChar;
-    }
 }
